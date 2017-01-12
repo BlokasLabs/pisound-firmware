0b29e0564f46fd7ad8600dbf034a15ffbf09b4ea
Giedrius Trainavicius <giedrius@blokas.io>
2017-01-12 02:35:48 +0200

    Updated flash.sh

--- flash.sh
+++ flash.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-pulseaudio -k
+set -e
+pulseaudio -k > /dev/null 2>&1 || true
 sudo modprobe -r snd_soc_pisound
 sudo avrdude -c linuxspi -p t841 -b 150000 -P /dev/spidev0.1 -U flash:w:pisound.hex
 sudo modprobe snd_soc_pisound
+sleep 1
 sh -c "pisound-btn >> ~/.pisound.log" &
 
