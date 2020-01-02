874bb472f876874c75b8d9ed1d16aa7fe05c638e
Giedrius <giedrius@blokas.io>
2020-01-02 15:23:41 +0000

    Pull SS pin low and stop alsa-state during firmware flashing

--- flash.sh
+++ flash.sh
@@ -11,10 +11,18 @@ try_unload() {
 set +e
 sudo systemctl stop pisound-btn
 sudo systemctl stop pisound-ctl
+sudo systemctl stop alsa-state || true
 pulseaudio -k > /dev/null 2>&1 || true
 try_unload
+# Pull SS pin low.
+sudo sh -c "echo 8 > /sys/class/gpio/export"
+sudo sh -c "echo out > /sys/class/gpio/gpio8/direction"
+sudo sh -c "echo 0 > /sys/class/gpio/gpio8/value"
 sudo avrdude -c linuxspi -p t841 -b 150000 -P /dev/spidev0.1 -U flash:w:pisound.hex
+# Restore SS pin.
+sudo sh -c "echo 8 > /sys/class/gpio/unexport"
 sudo modprobe snd_soc_pisound
 sleep 1
+sudo systemctl start alsa-state || true
 sudo systemctl start pisound-btn
 sudo systemctl start pisound-ctl
