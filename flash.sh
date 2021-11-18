36a340756eb2940d258a40a392332b73c0ddbf3c
Giedrius <giedrius@blokas.io>
2021-11-18 09:08:44 +0000

    Use dtoverlay and a special overlay to enable spidev0.0 and use it for firmware flashing

--- flash.sh
+++ flash.sh
@@ -9,25 +9,22 @@ try_unload() {
 }
 
 set +e
+sudo systemctl stop jack 2> /dev/null || true
 sudo systemctl stop pisound-btn 2> /dev/null
 sudo systemctl stop pisound-ctl 2> /dev/null
 sudo systemctl stop alsa-state 2> /dev/null || true
 aconnect -x
 pulseaudio -k > /dev/null 2>&1 || true
 try_unload
-# Pull SS pin low.
-if [ ! -e /sys/class/gpio/gpio8 ]; then
-	sudo sh -c "echo 8 > /sys/class/gpio/export"
-fi
-sudo sh -c "echo out > /sys/class/gpio/gpio8/direction"
-sudo sh -c "echo 0 > /sys/class/gpio/gpio8/value"
-sudo avrdude -c linuxspi -p t841 -b 150000 -P /dev/spidev0.1 -U flash:w:pisound.hex
-# Restore SS pin.
-sudo sh -c "echo 1 > /sys/class/gpio/gpio8/value"
+sudo dtoverlay pisound_spidev0_on.dtbo
+sudo avrdude -c linuxspi -p t841 -b 150000 -P /dev/spidev0.0 -U flash:w:pisound.hex
+sudo dtoverlay -r
+sudo modprobe -r snd_soc_pisound
 sudo modprobe snd_soc_pisound
 sleep 1
 sudo systemctl start alsa-state || true
 sudo systemctl start pisound-btn 2> /dev/null
 sudo systemctl start pisound-ctl 2> /dev/null
+sudo systemctl start jack 2> /dev/null || true
 echo
 echo "Please reboot your system now (sudo reboot). Thank you!"
